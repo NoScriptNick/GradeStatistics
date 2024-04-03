6760dc25321c4b427de8df25bfc94d42ee1c645f
NoScriptNick <144457708+NoScriptNick@users.noreply.github.com>
2024-04-03 09:20:07 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
-import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         // Creates a HashMap object
         HashMap<String, Integer> classGrades = new HashMap<String, Integer>();
 
         int classCount = 0;
+        String highClass ="";
+        String lowClass = "";
+        int high = -1;
+        int low = 1000;
+        int sum = 0;
+        int avg = 0;
         // Add keys and values (Class, Grade)
         classGrades.put("Comp", 97);
         classGrades.put("AP Calc", 99);
@@ -13,9 +18,23 @@ public class Main {
         classGrades.put("AP Physics", 100);
         classCount = classGrades.size();
         System.out.println("Grades by Subject:");
-        for (int i = 0; i < classGrades.size(); i++) {
-            System.out.println("%-10s %5d", classGrades.getKey(i), classGrades.getValue(i));
+        for (String subject: classGrades.keySet()) {
+            System.out.printf("\n%-20s %5d", subject, classGrades.get(subject));
+            if (classGrades.get(subject) < low) {
+                low = classGrades.get(subject);
+                lowClass = subject;
+            }
+            if (classGrades.get(subject) > high) {
+                high = classGrades.get(subject);
+                highClass = subject;
+            }
+            sum = sum + classGrades.get(subject);
         }
-        System.out.println("Number of Classes: " + classCount);
+        avg = sum/classCount;
+        System.out.println("\nNumber of Classes: " + classCount);
+        System.out.println("Highest Grade:" +"\n"+ highClass + ": " + high);
+        System.out.println("Lowest Grade:" +"\n"+ lowClass + ": " + low);
+        System.out.println("Average Grade: " + avg);
+
     }
 }
